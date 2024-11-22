e23ea4a5aa843969730a7bc1d607a4ce063c684f
Luisa Barrios <luisabarrios01@gmail.com>
2024-11-22 15:13:58 -0300

    Arreglos estéticos

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,26 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Poppins', 'Arial', 'sans-serif'],
+        serif: ['Merriweather', 'serif'],
+      },
+      animation: {
+        'zoom': 'zoom 2s ease-in-out infinite', // Animación de zoom continua
+      },
+      keyframes: {
+        zoom: {
+          '0%, 100%': {
+            transform: 'scale(1)', 
+          },
+          '50%': {
+            transform: 'scale(1.1)', 
+          }
+        }
+      }
+    
+    },
   },
   plugins: [],
 }
